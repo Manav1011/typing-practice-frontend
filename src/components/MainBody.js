2272754131446653842d5907a726479e67dfc555
manavshah <manavshah1011.ms@gmail.com>
2022-08-25 02:18:49 +0530

    Styling Done

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import DisplayPara from './DisplayPara'
+import {useState,useEffect} from 'react'
 import UserInteract from './UserInteract'
 
 
 const MainBody = ({Theme, ThemeText, ThemeBackground}) => {
+  const [ParaContent, setContent] = useState([]);
+  let para = `Life is a mixture of ups and downs and one who has life must have seen various colours of life. Sometimes the colours are vivid and bright and sometimes they are just black and white. Life is a challenge and one who has the courage and strength to face it bravely is the one who goes through it and emerges as a great and successful person in life. Every person who has life is given various opportunities to make his life happy and prosperous and the one who understands this surely succeed in life. People who think that life is easy must know how people who don’t have a home to live and food to eat survive on this planet.`;
+  useEffect(() => {
+    setContent(para.split(/(\s+)/).filter( e => e.trim().length > 0));
+  }, []);
   return (
     <div className={`${ThemeText}`} >
-        <DisplayPara Theme={Theme} ThemeBackground={ThemeBackground} />
-        <UserInteract Theme={Theme} ThemeBackground={ThemeBackground} />
+        <DisplayPara content={ParaContent} Theme={Theme} ThemeBackground={ThemeBackground} />
+        <UserInteract content={ParaContent} Theme={Theme} ThemeBackground={ThemeBackground} />
     </div>
   )
 }
--- src/components/UserInteract.js
+++ src/components/UserInteract.js
@@ -1,16 +1,116 @@
-import React from 'react'
+import {React,useState,useEffect,useRef} from 'react'
 import {Helmet} from 'react-helmet';
+import Swal from 'sweetalert2'
 
-const UserInteract = ({Theme , ThemeBackground}) => {
+const UserInteract = ({content, Theme , ThemeBackground}) => {
+
+  const [Counter, setCounter] = useState(0);
+  const [CorrectWords, setCorrectWords] = useState(0);
+  const [WrongWords, setWrongWords] = useState(0);  
+  const CorrectChars =useRef(0)
+  const [startTimer,setStartTimer]= useState(true)  
+  const [Showwpm,setshowwpm]=useState(false)
+  const [WPM ,setWPM]=useState(0)
+  const funRef = useRef(null);
+
+  const Timer = () => {    
+    var time = 59;
+    setStartTimer(false)
+    funRef.current= setInterval(function() {
+  var seconds = time ;
+  var minutes = (time - seconds) / 60;
+  if (seconds.toString().length == 1) {
+    seconds =  seconds;
+  }
+  document.getElementById("Timer").innerHTML =+ seconds;  
+  time--;
+  if (time < 0) {
+    document.getElementById("UserInput").readOnly = true
+    setshowwpm(true);    
+    clearInterval(funRef.current);    
+  }
+}, 1000);
+    }
+
+const onChangeHandler =(event) =>{    
+  document.getElementById(Counter).scrollIntoView();
+  let CurrentWord=content[Counter]+' '
+  let CurrentValue=event.target.value
+  let result=CurrentWord.includes(CurrentValue)
+  if (result){    
+    document.getElementById(Counter).classList.remove("bg-danger");
+  }
+  else{
+    document.getElementById(Counter).classList.add("bg-danger");    
+  }
+}
+
+  const OnSpaceHandler = (event) => {
+    if (startTimer){
+    Timer()
+    }   
+    document.getElementById(Counter).classList.add("bg-secondary");
+    if (event.keyCode === 32) {      
+      document.getElementById(Counter).classList.remove("bg-danger");
+      var value = event.target.value;
+      var CurrentWord=content[Counter]
+      for(let i=0; i<value.length ; i++){
+        if(value[i] === CurrentWord[i]){
+          CorrectChars.current = CorrectChars.current + 1
+        }
+        else{
+          if( value[i] != ' '){
+            CorrectChars.current = CorrectChars.current - value.length + 1
+          }
+        }
+      }
+      CorrectChars.current = CorrectChars.current + 1      
+      setWPM(CorrectChars.current / 5)          
+      if (value.length === 1) {
+        event.target.value = "";
+      } else {
+        setCounter(Counter + 1);        
+        if (event.target.value.trim() === content[Counter]) {
+          document.getElementById(Counter).classList.remove("bg-secondary");
+          document.getElementById(Counter + 1).classList.add("bg-secondary");
+          document.getElementById(Counter).classList.add("text-success");
+          setCorrectWords(CorrectWords + 1);
+        } else {
+          document.getElementById(Counter).classList.remove("bg-secondary");
+          document.getElementById(Counter + 1).classList.add("bg-secondary");
+          document.getElementById(Counter).classList.add("text-danger");
+          setWrongWords(WrongWords + 1);
+        }
+        event.target.value = "";
+      }
+    }
+  };
+
+  if(Showwpm){
+    Swal.fire({
+      icon: 'success',
+      title: 'Congratulations...',
+      text: `Your Speed Was ${WPM} Words Per Minute`,  
+    })
+  }
+
+  const ResetCounter = () => {
+    setCounter(0);
+  };
+
+
   return (
     <div className="mt-4 mx-5 UserInteraction">
          <Helmet>
                 <style>{`.UserInput {${ThemeBackground}}`}</style>
             </Helmet>
-      <form className='d-flex' role="search">
-        <input id='UserInput' autoFocus className={`form-control me-2 UserInput ${Theme == 'Light' ? 'text-dark' : 'text-light'}`} placeholder="Start Typing..." />                
-        <span className={`btn border ${Theme=='Light'? 'border-dark text-dark' : 'border-light text-light'}`} disabled >1:00</span>
-      </form>
+      <div className='d-flex'>
+        <input id='UserInput' autoFocus className={`form-control me-2 UserInput ${Theme == 'Light' ? 'text-dark' : 'text-light'}`} placeholder="Start Typing..." onChange= {(e) => onChangeHandler(e)} onKeyUp={(e) => OnSpaceHandler(e)}/>                
+        <span id="Timer" className={`me-2 btn border ${Theme=='Light'? 'border-dark text-dark' : 'border-light text-light'}`} disabled >1:00</span>
+        <button className="btn btn-primary" onClick={ResetCounter}>
+        <i className="bi bi-arrow-clockwise"></i>
+      </button>
+      </div>      
     </div>
   )
 }
