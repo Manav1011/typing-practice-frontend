dcd4b9e5ffef88a7a099c6c4f81084b379048a9c
manavshah <manavshah1011.ms@gmail.com>
2022-08-26 12:43:02 +0530

    Stylings 2

--- src/components/DisplayPara.js
+++ src/components/DisplayPara.js
@@ -8,11 +8,11 @@ function DisplayPara({content,Theme,ThemeBackground}) {
             <Helmet>
                 <style>{`.ParaCard {${ThemeBackground}}`}</style>
             </Helmet>
-      <Card className="ParaCard"  style={{fontSize: '1.5rem', maxHeight: '25vh' , overflow: 'scroll'}}>
+      <Card className="ParaCard"  style={{fontSize: '1.5rem', maxHeight: '20vh' , overflow: 'scroll'}}>
         <Card.Body> 
           {
             content.map( (value,index) => {
-                return <div className='fw-normal bg-opacity-25 pt-2' key={index} id={index} style={{display: 'inline-block'}}>{value} &nbsp;</div>
+                return <div className='fw-normal bg-opacity-50 pt-2' key={index} id={index} style={{display: 'inline-block'}}>{value} &nbsp;</div>
             })
         }
         
